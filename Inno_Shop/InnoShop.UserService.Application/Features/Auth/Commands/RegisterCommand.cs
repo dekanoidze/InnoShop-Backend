41fd2f40322ecaf61be5fbc30af8b42dca2024ff
dekanoidze <lukadekanoidze173@gmail.com>
2026-03-07 19:57:57 +0400

    Add confirm password and strengthen password validation rules

--- Inno_Shop/InnoShop.UserService.Application/Features/Auth/Commands/RegisterCommand.cs
+++ Inno_Shop/InnoShop.UserService.Application/Features/Auth/Commands/RegisterCommand.cs
@@ -12,5 +12,6 @@ namespace InnoShop.UserService.Application.Features.Auth.Commands
         public string Name { get; set; }=string.Empty;
         public string Password { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
