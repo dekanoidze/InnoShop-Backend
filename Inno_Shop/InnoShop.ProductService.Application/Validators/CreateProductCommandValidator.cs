f1cdb9796abf20a39d7ea95c4a3dfb1bbac34d23
dekanoidze <lukadekanoidze173@gmail.com>
2026-03-07 19:32:22 +0400

    Add FluentValidation for ProductService

---
+++ Inno_Shop/InnoShop.ProductService.Application/Validators/CreateProductCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using InnoShop.ProductService.Application.Features.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoShop.ProductService.Application.Validators
+{
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required")
+                .MaximumLength(100);
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required").MaximumLength(500);
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price can not be less then 0");
+        }
+    }
+}
---
+++ Inno_Shop/InnoShop.ProductService.Application/Validators/SearchProductsQueryValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using InnoShop.ProductService.Application.Features.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoShop.ProductService.Application.Validators
+{
+    public class SearchProductsQueryValidator : AbstractValidator<SearchProductsQuery>
+    {
+        public SearchProductsQueryValidator()
+        {
+            RuleFor(x => x.MinPrice).GreaterThanOrEqualTo(0)
+            .WithMessage("Minimum price can not be negative")
+            .When(x => x.MinPrice.HasValue);
+            RuleFor(x => x.MaxPrice).GreaterThanOrEqualTo(0)
+                .WithMessage("Maximum price can not be negative")
+                .When(x => x.MaxPrice.HasValue);
+            RuleFor(x => x.MinPrice)
+                .LessThanOrEqualTo(x => x.MaxPrice).WithMessage("Minimum price can not be greater then Maximum price")
+                .When(x => x.MinPrice.HasValue && x.MaxPrice.HasValue);
+
+        }
+
+    }
+}
