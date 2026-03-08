87ffc3eec994a7112b95fb3665f8b629d4fd75f2
dekanoidze <lukadekanoidze173@gmail.com>
2026-03-08 18:56:53 +0400

    Add unit tests for UserService and ProductService,also integration for UserService

---
+++ Inno_Shop/InnoShop.ProductService.Tests/Features/DeleteProductCommandHandlerTests.cs
@@ -0,0 +1,57 @@
+﻿using InnoShop.ProductService.Application.Interfaces;
+using InnoShop.ProductService.Domain.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using InnoShop.ProductService.Application.Features.Commands;
+
+namespace InnoShop.ProductService.Tests.Features
+{
+    public class DeleteProductCommandHandlerTests
+    {
+        private readonly Mock<IProductRepository> productRepositoryMock = new();
+
+        [Fact]
+        public async Task Handle_ShouldReturn_ProductDeletedSuccessfuly()
+        {
+            var product = new Product { Id = Guid.NewGuid(), Name = "Test Product" };
+            productRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(product);
+            productRepositoryMock.Setup(x => x.DeleteAsync(It.IsAny<Guid>())).Returns(Task.CompletedTask);
+
+            var handler = new DeleteProductCommandHandler
+                (
+                productRepositoryMock.Object
+                );
+            var command = new DeleteProductCommand
+            {
+                Id = product.Id
+            };
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.Should().BeTrue();
+        }
+        [Fact]
+        public async Task Handle_ShouldThrowExecption_WhenKeyNotFound()
+        {
+            productRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync((Product?)null);
+
+            var handler = new DeleteProductCommandHandler
+                (
+                productRepositoryMock.Object
+                );
+            var command = new DeleteProductCommand()
+            {
+                Id = Guid.NewGuid()
+            };
+
+            var act = async () => await handler.Handle(command, CancellationToken.None);
+
+            await act.Should().ThrowAsync<KeyNotFoundException>().WithMessage("*not found*");
+        }
+    }
+}
---
+++ Inno_Shop/InnoShop.UserService.Tests/Features/LoginCommandHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using BCrypt.Net;
+using FluentAssertions;
+using InnoShop.UserService.Application.Features.Auth.Commands;
+using InnoShop.UserService.Application.Interfaces;
+using InnoShop.UserService.Application.Services;
+using InnoShop.UserService.Domain.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoShop.UserService.Tests.Features
+{
+    public class LoginCommandHandlerTests
+    {
+        private readonly Mock<IUserRepository> userRepositoryMock = new();
+        private readonly Mock<IJwtService> jwtServiceMock = new();
+
+        [Fact]
+        public async Task Handle_ShouldReturnToken_WhenCredentialsAreValid()
+        {
+            var password = "Password123";
+            var user = new User
+            {
+                Email = "test@test.com",
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(password)
+            };
+            userRepositoryMock.Setup(x => x.GetByEmailAsync(It.IsAny<string>())).ReturnsAsync(user);
+            jwtServiceMock.Setup(x => x.GenerateToken(It.IsAny<User>())).Returns("fake_roken");
+
+            var handler = new LoginCommandHandler
+                (
+                userRepositoryMock.Object,
+                jwtServiceMock.Object
+                );
+            var command = new LoginCommand
+            {
+                Email = "test@test.com",
+                Password = "WrongPassword123"
+            };
+
+            var act = async () => await handler.Handle(command, CancellationToken.None);
+
+            await act.Should().ThrowAsync<UnauthorizedAccessException>().WithMessage("*Invalid email or password*");
+        }
+    }
+}
